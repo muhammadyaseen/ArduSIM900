b8b87e91beb8a4c99407931d0c546285900706f9
Muhammad Yaseen <mohammad_yaseen13@live.com>
2015-03-04 17:41:55 +0500

    Initial Commit

---
+++ ArduSIM900.ino
@@ -0,0 +1,131 @@
+/*
+* constants here
+*/
+
+bool interruptInProcess 	= false;
+baudRate 					= 115200;
+networkLED					= PIN_13;
+powerLED					= PIN_15;
+
+
+void setup()
+{
+	
+	setParameters();
+	
+	initSIM900();
+	
+	
+	
+}
+
+void loop()
+{
+
+	if ( interruptReceived() && !interruptInProcess)
+	{	
+		interruptInProcess = true;
+		
+		preCallSetup();
+		
+		makeCall();
+		
+		postCallSetup();
+		
+		interruptInProcess = false;
+	}
+}
+
+setParameters()
+{
+	//can set pin output modes, default values here
+	
+	Serial.begin(baudRate);
+	
+}
+
+
+initSIM900()
+{
+	//wait a few seconds for sim900 power on sequence to complete
+	delay(5000);
+	
+	//enter command mode
+	Serial.write("+++");
+	
+	//required for auto-bauding to figure out baud rate
+	Serial.write("AT");
+	
+	delay(3000);
+	
+	//switch to fixed bauding, and store the baud rate in non-volatile mem
+	switch (baudRate)
+	{
+		case 9600:
+			Serial.write("AT+IPR=9600;&W");
+			
+		case 38400:
+			Serial.write("AT+IPR=38400;&W");
+		
+		case 115200:
+			Serial.write("AT+IPR=115200;&W");
+		
+		default:
+			Serial.write("AT+IPR=9600;&W");
+	}
+	
+	delay(2000);
+	
+}
+
+preCallSetup() {
+
+	
+}
+
+makeCall() {
+	
+	Serial.write("AT+CHFA=0");
+	
+	Serial.write("ATL3");
+	
+	Serial.write("AT+CMUT=1");
+	
+	Serial.write("ATD+9234122xxxxx");
+	
+	delay(30000);
+	
+	Serial.write("ATH");
+}
+
+postCallSetup() {
+
+}
+
+void sendSMS() {}
+
+void onSMSRecvd() {}
+
+void saveNumberInPhonebook() {}
+
+void queryModuleInfo() {
+	
+	Serial.write("Firmware info : \n ");
+	Serial.write("AT+GMR");
+	
+	Serial.write("IMEI : \n");
+	Serial.write("AT+GSN");
+	
+	Serial.write("SIM number : \n");
+	Serial.write("AT+CNUM?");
+	
+}
+
+bool interruptReceived() {
+
+	//TODO: implement switch check logic
+	
+	return true;
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ArduSIM900
-Arduino UNO compatible code for interfacing with Sim900 GSM Module
+Arduino UNO compatible code for interfacing with Sim900 GSM Module. This is a work in progress.
